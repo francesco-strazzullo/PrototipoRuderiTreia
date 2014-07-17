d7a0351387e15fb949db60192dd03c6bf7283074
francescostrazzullo <francescostrazzullo@192.168.0.10>
2014-07-17 21:52:25 +0200

    commit iniziale

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ruderiTreia001App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ruderiTreia001App
+ */
+angular.module('ruderiTreia001App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ruderiTreia001App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ruderiTreia001App
+ */
+angular.module('ruderiTreia001App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
