1491e584b7664b551ff03fb7bdb3d470927b9107
francescostrazzullo <francescostrazzullo@192.168.0.10>
2014-07-17 23:41:10 +0200

    sistemati controllers

---
+++ app/scripts/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('ruderiTreia001App')
+        .controller('root', ['$scope',function($scope) {
+              $scope.backgroundImage = 'images/sanvicino.jpg';
+        }])
+        .controller('home', ['$scope',function($scope) {
+              
+        }]);
